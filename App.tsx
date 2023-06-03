a918584edc4de9173da325236763df6cef387a28
reimibeta <65539218+reimibeta@users.noreply.github.com>
2023-06-03 18:47:59 +0700

    .

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import { multiply } from "react-native-pin-lock-view";
 import { multiply } from "react-native-pin-lock-view";
 
 
